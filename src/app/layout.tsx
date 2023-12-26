5cf3cf1ddc471293702b2273002a0f113325039a
Mateus Queirós <mateuscqueiros1@gmail.com>
2023-12-26 13:32:41 -0300

    fix global styles import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import '@/styles/fonts.css';
+import '@/styles/globals.css';
 import type { Metadata } from 'next';
-import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Nubank',
